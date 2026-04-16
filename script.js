8b817dc344e3ede2e85ba07d1ff5fd5cef37d2e0
lupianricardo <r.ramirez.lupian@gmail.com>
2026-04-15 22:47:47 -0600

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function abrirZoom(elemento) {
     const modal = document.getElementById("modalZoom");
     const imgModal = document.getElementById("imgExpandida");
-    const imagenOriginal = elemento.querySelector("img").src;
+    const textoModal = document.getElementById("nombreAlumnoZoom");
     
+    // Obtenemos los datos de la tarjeta clicada
+    const rutaImagen = elemento.querySelector("img").src;
+    const nombreAlumno = elemento.querySelector("strong").innerText;
+    const tituloTrabajo = elemento.querySelector("h3").innerText;
+    
+    // Asignamos los datos al modal
     modal.style.display = "block";
-    imgModal.src = imagenOriginal;
+    imgModal.src = rutaImagen;
+    textoModal.innerText = tituloTrabajo + " - Por: " + nombreAlumno;
 }
 
 function cerrarZoom() {
     document.getElementById("modalZoom").style.display = "none";
 }
+
+// Cerrar también con la tecla Escape
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") {
+        cerrarZoom();
+    }
+});
